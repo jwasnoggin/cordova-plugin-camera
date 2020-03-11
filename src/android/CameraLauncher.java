9246470d2e5128a35f8553a66de24687930e42ac
James Wassink <jwassink@noggin.com.au>
2020-03-11 14:32:37 +1100

    Fix arg index

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -169,7 +169,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
             this.allowEdit = args.getBoolean(7);
             this.correctOrientation = args.getBoolean(8);
 			this.saveToPhotoAlbum = args.getBoolean(9);
-			this.preserveGalleryFormat = args.getBoolean(10);
+			this.preserveGalleryFormat = args.getBoolean(12);
 
             // If the user specifies a 0 or smaller width/height
             // make it -1 so later comparisons succeed
